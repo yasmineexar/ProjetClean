3b5ac6bfe8f190072f91a93ec5d7b2492d26595d
Lydia KADOUCHE <lydia.kadouche.dz@viacesi.fr>
2020-12-01 19:27:44 +0100

    Personnel

--- ProjetClean/SVC_Gcommande.h
+++ ProjetClean/SVC_Gcommande.h
@@ -1,7 +1,26 @@
 #pragma once
-namespace Service {
-	ref class SVC_Gcommande
+#include "CAD.h"
+#include "Personne.h"
+#include "Adresse.h"
+using namespace System::Data;
+using namespace System::Data::SqlClient;
+using namespace System::Xml;
+using namespace System;
+namespace Composant
+{
+	ref class SVC_Gemploye
 	{
+	private:
+		Composant::CAD^ cad;
+		Composant::Personne^ personne;
+	    Composant::Adresse^ adresse;
+		DataSet^ ds;
+	public:
+		SVC_Gemploye(void);
+		DataSet^ listePersonne(String^);
+		DataSet^ adressesPersonne(int, String^);
+		void ajouter(String^, String^, array<String^>^);
+		void modifier(int, String^, String^, array<String^>^);
+		void supprimer(int);
 	};
 }
-
