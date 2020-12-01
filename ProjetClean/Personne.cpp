372f53ca8069fe5688bf5d5c341ff4d8d7d19d11
nazim kadouche <kadouche.n@gmail.com>
2020-12-01 11:00:21 +0100

    personne

---
+++ ProjetClean/Personne.cpp
@@ -0,0 +1,59 @@
+#include "Personne.h"
+namespace Composant {
+    
+
+    Composant::Personne::Personne()
+    {
+        
+    }
+
+    void Composant::Personne::set_nom(String^ nom)
+    {
+        this->nom = nom;
+    }
+
+    void Composant::Personne::set_prenom(String^ prenom)
+    {
+        this->prenom = prenom;
+    }
+
+    String^ Composant::Personne::get_nom()
+    {
+        return this->nom;
+    }
+
+    String^ Composant::Personne::get_prenom()
+    {
+        return this->prenom;
+    }
+
+    void Composant::Personne::set_id(int id)
+    {
+        this->id = id;
+    }
+
+    int Composant::Personne::get_id()
+    {
+        return this->id;
+    }
+
+    String^ Composant::Personne::SELECT(void)
+    {
+        
+    }
+
+    String^ Composant::Personne::INSERT(void)
+    {
+
+    }
+
+    String^ Composant::Personne::UPDATE(void)
+    {
+
+    }
+
+    String^ Composant::Personne::DELETE(void)
+    {
+
+    }
+}
---
+++ ProjetClean/Personne.h
@@ -0,0 +1,24 @@
+#pragma once
+namespace Composant {
+	using namespace System;
+	ref class Personne
+	{
+		protected:
+			String^ nom,^prenom;
+			int id;
+		public:
+			Personne();
+			void set_nom(String^ nom);
+			void set_prenom(String^ prenom);
+			String^ get_nom();
+			String^ get_prenom();
+			void set_id(int id);
+			int get_id();
+			virtual String^ SELECT(void);
+			virtual String^ INSERT(void);
+			virtual String^ UPDATE(void);
+			virtual String^ DELETE(void);
+
+	};
+}
+
