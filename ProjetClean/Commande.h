2687559edebf277752bb7cf8496c4301c687ee0b
nazim kadouche <kadouche.n@gmail.com>
2020-12-01 15:44:21 +0100

    commande.h

--- ProjetClean/Commande.h
+++ ProjetClean/Commande.h
@@ -7,6 +7,17 @@ namespace Composant {
 			System::DateTime^ date_emission, ^ date_livraison, ^ date_reglement_solde;
 			System::String^ ref;
 		public:
+			Commande();
+			void set_id_commande(int);
+			void set_quantite_article(int);
+			void set_id_client(int);
+			void set_id_adresse_livraison(int);
+			void set_
+			void set_
+			void set_
+			void set_
+			void set_
+			void set_
 	};
 }
 
