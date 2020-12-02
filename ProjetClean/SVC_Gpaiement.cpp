2844be07f424013321c9268ce7dc1f45b696fff3
Maryam CHENAOUI <maryam.chenaoui.dz@viacesi.fr>
2020-12-02 23:56:31 +0100

    Gpaiement

---
+++ ProjetClean/SVC_Gpaiement.cpp
@@ -0,0 +1,36 @@
+#include "SVC_Gpaiement.h"
+namespace Service {
+	SVC_Gpaiement::SVC_Gpaiement()
+	{
+		this->cad = gcnew Composant::CAD();
+		this->paiement = gcnew Composant::Paiement();
+	}
+	DataTable^ SVC_Gpaiement::Paiement()
+	{
+		throw gcnew System::NotImplementedException();
+		// TODO: insérer une instruction return ici
+	}
+	//une méthode qui retourne un DataTable contenant la liste des clients
+	DataTable^ SVC_Gpaiement::Paiement()
+	{
+		this->dt = gcnew DataTable();
+		this->dt = this->cad->getRows(this->paiement->SELECT());
+		return this->dt;
+	}
+	void SVC_Gpaiement::afficher(int)
+	{
+		throw gcnew System::NotImplementedException();
+	}
+	void SVC_Gpaiement::ajouter(int ID_Paiement, DateTime^ Date_Paiement, DateTime^ Date_ReglementSolde, String^ Moyen_Paiement, float Montant_Total_HT, float Montant_Total_TVA, int ID_Client, int ID_Commande)
+	{
+		throw gcnew System::NotImplementedException();
+	}
+	void SVC_Gpaiement::modifier(int ID_Paiement, DateTime^ Date_Paiement, DateTime^ Date_ReglementSolde, String^ Moyen_Paiement, float Montant_Total_HT, float Montant_Total_TVA, int ID_Client, int ID_Commande)
+	{
+		throw gcnew System::NotImplementedException();
+	}
+	void SVC_Gpaiement::supprimer(int)
+	{
+		throw gcnew System::NotImplementedException();
+	}
+}
---
+++ ProjetClean/SVC_Gpaiement.h
@@ -0,0 +1,25 @@
+#pragma once
+#include"CAD.h"
+#include"Paiement.h"
+#include"Client.h"
+#include"Commande.h"
+namespace Service {
+	using namespace System::Data;
+	using namespace System::Data::SqlClient;
+	using namespace System::Xml;
+	using namespace System;
+	ref class SVC_Gpaiement {
+	private:
+		Composant::CAD^ cad;
+		Composant::Paiement^ paiement;
+		DataTable^ dt;
+
+	public:
+		SVC_Gpaiement();
+		DataTable^ Paiement();
+		void afficher(int);
+		void ajouter(int, DateTime^, DateTime^, String^, float, float, int, int);
+		void modifier(int, DateTime^, DateTime^, String^, float, float, int, int);
+		void supprimer(int);
+	};
+}
