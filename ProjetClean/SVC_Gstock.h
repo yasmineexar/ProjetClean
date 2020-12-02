e79277f703b2c6bfd830c60005d62ac21ff1d5d7
Lydia KADOUCHE <lydia.kadouche.dz@viacesi.fr>
2020-12-02 22:57:24 +0100

    Bon hada khalouta

--- ProjetClean/SVC_Gstock.h
+++ ProjetClean/SVC_Gstock.h
@@ -1,6 +1,24 @@
-#pragma once
+﻿#pragma once
+#include"CAD.h"
+#include"Article.h"
 namespace Service {
-	ref class SVC_Gstock
-	{
-	};
-}
+using namespace System::Data;
+using namespace System::Data::SqlClient;
+using namespace System::Xml;
+using namespace System;
+ref class SVC_Gstock
+{
+private:
+	Composant::CAD^ cad;
+	Composant:: Article^ article;
+	DataTable^ dArticle;
+public:
+	SVC_Gstock();
+	DataTable^ listestock();
+	void afficher(int);
+	void ajouter(String^, String^, DateTime^, DateTime);
+	void modifier(int, String^, String^, DateTime^, DateTime);
+	void supprimer(int);
+}​​;
+}​​
+
